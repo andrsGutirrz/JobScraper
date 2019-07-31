deb30c2b2ffbcf272f7e620f6d1366f1ce724ada
andrsGutirrz <andres.gutierrez.arcia@gmail.com>
2019-07-31 00:10:28 -0600

    jupyter

--- common.py
+++ common.py
@@ -4,8 +4,9 @@ import yaml
 __config = None
 
 def config():
+    """YAML FILE READER"""
     global __config
     if not __config:
         with open('config.yaml',mode='r') as f:
             __config = yaml.load(f)
-    return __config
+    return __config        
